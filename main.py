8063e0681cf66152357f6fa7f99be9a35ab9408e
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-08-08 15:18:18 +0530

    code updated

---
+++ main.py
@@ -0,0 +1,35 @@
+# Website Blocker using Python
+
+import datetime
+import time
+
+end_time = datetime.datetime(2022,8,5)
+
+site_block = ["https://www.wscubetech.com", "https://www.facebook.com"]
+host_path = "C:/Windows/System32/drivers/etc/hosts"
+redirect = "127.0.0.1"
+
+while True:
+    if datetime.datetime.now()<end_time:
+        print("Start Blocking")
+        with open(host_path, "r+") as host_file:
+            content = host_file.read()
+            for website in site_block:
+                if website not in content:
+                    host_file.write(redirect + " " + website + "\n")
+                else:
+                    pass    
+    else:
+        with open(host_path, "r+") as host_file:
+            content = host_file.readlines()
+            host_file.seek(0)
+            for lines in content:
+                if not any(website in lines for website in site_block):
+                    host_file.write(lines)
+                    
+            host_file.truncate()  
+        
+        time.sleep(5) #delay          
+
+        
+# Yash Sanjeev Halwai        
